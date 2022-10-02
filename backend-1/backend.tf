caed6d672a55a2675ec1b6f418203a566941671b
carlosayol <Carlosaolar@gmail.com>
2022-10-02 11:44:17 -0500

    setup encryption for s3 files

--- backend-1/backend.tf
+++ backend-1/backend.tf
@@ -1,7 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "backend-bucket-1-test"
-    key    = "dev"
-    region = "us-east-2"
+    bucket     = "backend-bucket-1-test"
+    key        = "dev"
+    region     = "us-east-2"
+    encrypt    = true
+    kms_key_id = "arn:aws:kms:us-east-2:427168880860:key/6c79b595-228f-4278-bc59-66b6d3c80023"
   }
 }
--- backend-1/main.tf
+++ backend-1/main.tf
@@ -1,13 +1,44 @@
+# provider "aws" {
+#   region = "us-east-2"
+# }
+
+# resource "aws_s3_bucket" "backend-1" {
+#   bucket = var.bucket_name
+#   tags   = var.tags
+# }
+
+# resource "aws_s3_bucket_acl" "backend-1-acl" {
+#   bucket = aws_s3_bucket.backend-1.id
+#   acl    = var.acl
+# }
+
 provider "aws" {
   region = "us-east-2"
+}
+
+resource "aws_kms_key" "mykey" {
+  description             = "Key State File"
+  deletion_window_in_days = 10
 }
 
 resource "aws_s3_bucket" "backend-1" {
   bucket = var.bucket_name
   tags   = var.tags
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.mykey.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
 }
 
 resource "aws_s3_bucket_acl" "backend-1-acl" {
   bucket = aws_s3_bucket.backend-1.id
   acl    = var.acl
+}
+
+output "arn" {
+  value = aws_kms_key.mykey.arn
 }
